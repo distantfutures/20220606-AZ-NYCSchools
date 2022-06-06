970045f9ad545e7fa3509f9fb1db26cf7004091d
Andrew Zheng <andzheng@gmail.com>
2022-06-06 09:16:49 -0400

    First commit

---
+++ app/src/main/java/com/distantfutures/a20220606_az_nycschools/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.distantfutures.a20220606_az_nycschools
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
